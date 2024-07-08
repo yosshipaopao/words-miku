f95a043015670c14a20ddfab2e653cb29dc78824
yosshipaopao <87005177+yosshipaopao@users.noreply.github.com>
2024-07-09 01:11:46 +0900

    v0

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 "use strict";
-import {min, max, Word, Words, mouse, colors} from "./utils.ts";
+import "./style.css";
+import {colors, fonts, max, min, mouse, songs, Word, Words} from "./utils.ts";
+import type {IVideo} from "textalive-app-api";
 import {IChar, IPhrase, IWord, Player} from "textalive-app-api";
 import Konva from "konva";
-import type {IPlayerApp, IVideo} from "textalive-app-api";
 import hexRgb from "hex-rgb";
 import rgbHex from "rgb-hex";
+
+const song_select = document.getElementById("song_select") as HTMLSelectElement;
+const song_select_btn = document.getElementById("song_select_btn") as HTMLButtonElement;
+
+for (let i = 0; i < songs.length; i++) {
+    const idx = i;
+    const song = songs[i];
+    const option = document.createElement("option");
+    option.textContent = song.title;
+    option.value = idx.toString();
+    song_select.add(option);
+}
+song_select_btn.onclick = async () => {
+    const song = songs[parseInt(song_select.value)];
+    await player.createFromSongUrl(song.url, song.options);
+}
 
 window.onresize = () => {
     if (stage) {
@@ -44,21 +61,7 @@ player.addListener({
     },
     //onThrottledTimeUpdate: (position: number) =>
     //    console.log("再生位置のアップデート:", position, "ミリ秒"),
-    onAppReady: (app: IPlayerApp) => {
-        if (!app.managed) {
-            player.createFromSongUrl("https://piapro.jp/t/ELIC/20240130010349", {
-                video: {
-                    // 音楽地図訂正履歴
-                    beatId: 4592299,
-                    chordId: 2727639,
-                    repetitiveSegmentId: 2824330,
-                    // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FELIC%2F20240130010349
-                    lyricId: 59419,
-                    lyricDiffId: 13966,
-                },
-            });
-        }
-    },
+    //onAppReady: (app: IPlayerApp) => {},
     onVideoReady: (v: IVideo) => {
         base_size = min(window.innerWidth / 16, window.innerHeight / 9);
         stage = new Konva.Stage({
@@ -80,8 +83,6 @@ player.addListener({
         main_layer.add(focus_rect);
         make_bg(bg_layer);
         stage.add(bg_layer);
-
-
         // Phrase
         words_groups.push(create_words_group(v.firstPhrase));
         // Word
@@ -109,7 +110,7 @@ player.addListener({
             fill: "#000000aa",
         });
         color_canvas_layer.hide();
-        canvas_layer_bg.on("click", ()=>{
+        canvas_layer_bg.on("click", () => {
             color_canvas_layer.hide();
         });
         color_canvas_layer.add(canvas_layer_bg);
@@ -127,24 +128,24 @@ player.addListener({
         });
         color_canvas_group.add(color_canvas_window_rect);
         color_canvas_rect = new Konva.Rect({
-            x: stage.width() / 16 /stage.scaleX(),
-            y: stage.width() / 16 /stage.scaleY(),
-            width: stage.width()  / 6 /stage.scaleX(),
-            height: stage.width() / 6 /stage.scaleY(),
+            x: stage.width() / 16 / stage.scaleX(),
+            y: stage.width() / 16 / stage.scaleY(),
+            width: stage.width() / 6 / stage.scaleX(),
+            height: stage.width() / 6 / stage.scaleY(),
             fill: "white",
             stroke: "black",
             strokeWidth: 10
         });
         color_canvas_group.add(color_canvas_rect);
         color_canvas_layer.add(color_canvas_group);
-        for(const i of [0,1,2]) {
+        for (const i of [0, 1, 2]) {
             const r_group = new Konva.Group({
                 x: (stage.width() / 3.5 + stage.width() / 15 * i) / stage.scaleX(),
                 y: stage.width() / 16 / stage.scaleY()
             });
             const r_bar = new Konva.Line({
                 points: [0, 0, 0, base_size * 2.5 / stage.scaleY()],
-                stroke: 'red',
+                stroke: ["red", "blue", "green"][i],
                 strokeWidth: 10,
                 lineCap: 'round',
                 lineJoin: 'round',
@@ -163,13 +164,13 @@ player.addListener({
             r_point.on("dragmove", () => {
                 r_point.x(0);
                 r_point.y(min(max(r_point.y(), 0), base_size * 2.5 / stage.scaleY()));
-                if(!selected_color_rect)return;
-                const rgb = hexRgb(selected_color_rect.fill().toString(),{format:"array"});
+                if (!selected_color_rect) return;
+                const rgb = hexRgb(selected_color_rect.fill().toString(), {format: "array"});
                 rgb[i] = r_point.y() / (base_size * 2.5 / stage.scaleY()) * 255;
-                const hex = "#"+rgbHex(rgb[0],rgb[1],rgb[2],rgb[3]);
+                const hex = "#" + rgbHex(rgb[0], rgb[1], rgb[2], rgb[3]);
                 selected_color_rect.fill(hex);
                 color_canvas_rect.fill(hex);
-                if(focussing_shape)focussing_shape.fill(hex);
+                if (focussing_shape) focussing_shape.fill(hex);
             });
             r_group.add(r_point);
             color_canvas_points.push(r_point);
@@ -191,14 +192,14 @@ const make_bg = (bg_layer: Konva.Layer) => {
     const up_line = new Konva.Rect({
         width: stage.width() / stage.scaleX(),
         height: base_size / stage.scaleX(),
-        fill: 'yellow'
+        fill: '#008899'
     });
     bg_layer.add(up_line);
     const down_line = new Konva.Rect({
         width: stage.width() / stage.scaleX(),
         height: base_size / stage.scaleX(),
         y: (stage.height() - base_size) / stage.scaleY(),
-        fill: 'yellow'
+        fill: '#008899'
     });
     bg_layer.add(down_line);
     main_rect = new Konva.Rect({
@@ -206,8 +207,8 @@ const make_bg = (bg_layer: Konva.Layer) => {
         height: stage.height() * 7 / 9 / stage.scaleX(),
         x: stage.width() / 9 / stage.scaleX(),
         y: base_size / stage.scaleY(),
-        fill: "blue",
-        stroke: "black",
+        fill: "#ffffff",
+        stroke: "#000000",
         strokeWidth: 4,
     });
     main_rect.on("click", () => change_focus(main_rect));
@@ -225,6 +226,12 @@ const make_bg = (bg_layer: Konva.Layer) => {
     });
     make_colorpalet(colorpalet_group);
     bg_layer.add(colorpalet_group);
+    const font_control_group = new Konva.Group({
+        x: stage.width() * 8 / 9 / stage.scaleX(),
+        y: base_size / stage.scaleY() + stage.width() / 3 / stage.scaleY(),
+    });
+    make_font_control(font_control_group);
+    bg_layer.add(font_control_group);
     const transform_control_group = new Konva.Group({
         x: stage.width() / 2 / 9 / stage.scaleX(),
         y: base_size / stage.scaleY() + 20,
@@ -233,65 +240,105 @@ const make_bg = (bg_layer: Konva.Layer) => {
     bg_layer.add(transform_control_group);
 }
 const make_control = (control_group: Konva.Group) => {
+    // Change Mode
+    const change_mode_group = new Konva.Group({
+        x: stage.width() / 18 / stage.scaleX()
+    });
+    change_mode_group.on("mouseover", mouse.pointer);
+    change_mode_group.on("mouseout", mouse.default);
+    change_mode_group.on("click", () => {
+        change_words_mode((words_mode + 1) % 3);
+    });
     const change_mode_button = new Konva.Circle({
         width: base_size * .8 / stage.scaleX(),
         height: base_size * .8 / stage.scaleX(),
-        x: stage.width() / 18 / stage.scaleX(),
         fill: 'white',
         stroke: 'black',
         strokeWidth: 4 / stage.scaleX(),
     });
-    change_mode_button.on("mouseover", mouse.pointer);
-    change_mode_button.on("mouseout", mouse.default);
-    change_mode_button.on("click", () => {
-        change_words_mode((words_mode + 1) % 3);
+    change_mode_group.add(change_mode_button);
+    Konva.Image.fromURL("/switch.png", (Node) => {
+        Node.setAttrs({
+            width: base_size * .666 / stage.scaleX(),
+            height: base_size * .666 / stage.scaleX(),
+            offsetX: base_size / 3 / stage.scaleX(),
+            offsetY: base_size / 3 / stage.scaleY()
+        });
+        change_mode_group.add(Node);
     });
-    control_group.add(change_mode_button);
+    control_group.add(change_mode_group);
+
+    // Play
+    const play_group = new Konva.Group({
+        x: stage.width() / 9 / stage.scaleX(),
+    })
+    play_group.on("mouseover", mouse.pointer);
+    play_group.on("mouseout", mouse.default);
+    play_group.on("click", () => {
+        if (player.video) player.isPlaying ? player.requestPause() : player.requestPlay();
+    });
     const play_button = new Konva.Circle({
         width: base_size * .8 / stage.scaleX(),
         height: base_size * .8 / stage.scaleX(),
-        x: stage.width() / 9 / stage.scaleX(),
         offsetX: -base_size / 2 / stage.scaleX(),
         fill: 'white',
         stroke: 'black',
         strokeWidth: 4 / stage.scaleX(),
     });
-    play_button.on("mouseover", mouse.pointer);
-    play_button.on("mouseout", mouse.default);
-    play_button.on("click", () => {
-        if (player.video) player.isPlaying ? player.requestPause() : player.requestPlay();
+    play_group.add(play_button);
+    const play_triangle = new Konva.RegularPolygon({
+        x: base_size / 2 / stage.scaleX(),
+        fill: "black",
+        sides: 3,
+        radius: 13 / stage.scaleX(),
+        rotation: 90
     });
-    control_group.add(play_button);
-    const capture_button = new Konva.Circle({
-        width: base_size * .8 / stage.scaleX(),
-        height: base_size * .8 / stage.scaleX(),
+    play_group.add(play_triangle);
+    control_group.add(play_group);
+    // Capture
+    const capture_group = new Konva.Group({
         x: stage.width() * 8.5 / 9 / stage.scaleX(),
-        fill: 'white',
-        stroke: 'black',
-        strokeWidth: 4 / stage.scaleX(),
     });
-    capture_button.on("mouseover", mouse.pointer);
-    capture_button.on("mouseout", mouse.default);
-    capture_button.on("click", () => {
+    capture_group.on("mouseover", mouse.pointer);
+    capture_group.on("mouseout", mouse.default);
+    capture_group.on("click", () => {
         main_rect.strokeEnabled(false);
         change_focus(main_rect);
-        (document.getElementById("img") as HTMLImageElement).src = stage.toDataURL({
+        img_el.src = stage.toDataURL({
             width: stage.width() * 7 / 9,
             height: stage.height() * 7 / 9,
             x: stage.width() / 9,
             y: base_size,
             pixelRatio: Math.ceil(1920 / stage.width())
         });
+        show_share_modal();
         main_rect.strokeEnabled(true);
     });
-    control_group.add(capture_button);
+    const capture_button = new Konva.Circle({
+        width: base_size * .8 / stage.scaleX(),
+        height: base_size * .8 / stage.scaleX(),
+        fill: 'white',
+        stroke: 'black',
+        strokeWidth: 4 / stage.scaleX(),
+    });
+    capture_group.add(capture_button);
+    Konva.Image.fromURL("/camera.png", (Node) => {
+        Node.setAttrs({
+            width: base_size * .666 / stage.scaleX(),
+            height: base_size * .666 / stage.scaleX(),
+            offsetX: base_size / 3 / stage.scaleX(),
+            offsetY: base_size / 3 / stage.scaleY()
+        });
+        capture_group.add(Node);
+    });
+    control_group.add(capture_group);
 
     const audio_control = new Konva.Group({
         x: stage.width() * 7 / 9 / stage.scaleX()
     });
     const audio_bar = new Konva.Line({
         points: [0, 0, stage.width() / 9 / stage.scaleX(), 0],
-        stroke: 'red',
+        stroke: '#eeaa00',
         strokeWidth: 10,
         lineCap: 'round',
         lineJoin: 'round',
@@ -319,7 +366,7 @@ const make_control = (control_group: Konva.Group) => {
     });
     const progress_bar = new Konva.Line({
         points: [0, 0, stage.width() / 2 / stage.scaleX(), 0],
-        stroke: 'red',
+        stroke: '#eeaa00',
         strokeWidth: 10,
         lineCap: 'round',
         lineJoin: 'round',
@@ -371,10 +418,43 @@ const make_colorpalet = (colorpalet_group: Konva.Group) => {
         rect.on("click", () => {
             focussing_shape.fill(rect.fill());
         });
-        rect.on("dblclick",()=>{
+        rect.on("dblclick", () => {
             open_color_canvas(rect);
         });
         colorpalet_group.add(rect);
+    }
+}
+const make_font_control = (font_control_group: Konva.Group) => {
+    const size = stage.width() / 2 / 9 / stage.scaleX();
+    for (let i = 0; i < fonts.length; i++) {
+        const font = fonts[i];
+        const group = new Konva.Group({
+            x: (i % 2 == 0 ? 0 : 1) * (size) + size * .1,
+            y: (Math.floor(i / 2)) * size,
+        });
+        group.on("mouseenter", mouse.pointer);
+        group.on("mouseleave", mouse.default);
+        group.on("click", () => {
+            if (focussing_shape == main_rect) return;
+            (focussing_shape as Konva.Text).fontFamily(font);
+        });
+        const rect = new Konva.Rect({
+            width: size * .8,
+            height: size * .8,
+            fill: "white",
+            stroke: "black",
+            strokeWidth: 4
+        });
+        group.add(rect);
+
+        const text = new Konva.Text({
+            fill: "black",
+            fontFamily: font,
+            fontSize: size * .8,
+            text: "あ"
+        });
+        group.add(text);
+        font_control_group.add(group);
     }
 }
 const make_transform_control = (transform_control_group: Konva.Group) => {
@@ -383,7 +463,7 @@ const make_transform_control = (transform_control_group: Konva.Group) => {
     });
     const size_control_bar = new Konva.Line({
         points: [0, 0, 0, base_size * 2.5 / stage.scaleY()],
-        stroke: 'red',
+        stroke: '#ffee77',
         strokeWidth: 10,
         lineCap: 'round',
         lineJoin: 'round',
@@ -407,7 +487,6 @@ const make_transform_control = (transform_control_group: Konva.Group) => {
             new_scale *= size_control_point.y() / (base_size * 2.5 / stage.scaleY());
             new_scale += .1;
             focussing_shape.scale({x: new_scale, y: new_scale});
-            console.log("scale shape");
             transform_focus_rect();
         }
     });
@@ -418,7 +497,7 @@ const make_transform_control = (transform_control_group: Konva.Group) => {
     });
     const rotate_control_bar = new Konva.Line({
         points: [0, 0, 0, base_size * 2.5 / stage.scaleY()],
-        stroke: 'red',
+        stroke: '#ffee77',
         strokeWidth: 10,
         lineCap: 'round',
         lineJoin: 'round',
@@ -463,6 +542,7 @@ const create_words_group = (p: IPhrase | IWord | IChar): [Konva.Group, Words, nu
                 t.text.fill(focussing_shape.fill());
                 t.text.rotation(focussing_shape.rotation());
                 t.text.scale(focussing_shape.scale());
+                t.text.fontFamily((focussing_shape as Konva.Text).fontFamily());
             }
             t.text.absolutePosition(stage.getPointerPosition() ?? {x: 0, y: 0});
         });
@@ -528,13 +608,14 @@ const transform_focus_rect = () => {
     focus_rect.offsetX(focussing_shape.offsetX() * focussing_shape.scaleX() + 4 / stage.scaleX());
     focus_rect.offsetY(focussing_shape.offsetY() * focussing_shape.scaleY() + 4 / stage.scaleY());
 }
-const open_color_canvas = (rect:Konva.Rect)=>{
+const open_color_canvas = (rect: Konva.Rect) => {
     selected_color_rect = rect;
-    const rgb = hexRgb(selected_color_rect.fill().toString(),{format:"array"});
+    const rgb = hexRgb(selected_color_rect.fill().toString(), {format: "array"});
     color_canvas_rect.fill(rect.fill());
-    for(let i=0;i<3;i++)color_canvas_points[i].y(rgb[i]/255*(base_size * 2.5 / stage.scaleY()));
+    for (let i = 0; i < 3; i++) color_canvas_points[i].y(rgb[i] / 255 * (base_size * 2.5 / stage.scaleY()));
     color_canvas_layer.show();
 }
+
 let base_size: number;
 let stage: Konva.Stage;
 let main_layer: Konva.Layer;
@@ -553,7 +634,44 @@ let color_canvas_rect: Konva.Rect;
 let color_canvas_points: Konva.Circle[] = [];
 let selected_color_rect: Konva.Rect;
 
-// todo フォント選択
-// todo 補助ライン
-// todo 曲選択
-// todo シェア
+// todo 補助ライン <- なし
+
+const img_el = document.getElementById("img") as HTMLImageElement;
+const shareBG = document.getElementById("bg");
+const shareContent = document.getElementById("share");
+shareBG!.onclick = () => {
+    shareBG!.style.display = "none";
+    shareContent!.style.display = "none";
+}
+const show_share_modal = () => {
+    shareBG!.style.display = "block";
+    shareContent!.style.display = "flex";
+}
+const save_btn = document.getElementById("save_btn") as HTMLButtonElement;
+const copy_btn = document.getElementById("copy_btn") as HTMLButtonElement;
+const share_btn = document.getElementById("share_btn") as HTMLButtonElement;
+
+
+save_btn.onclick = () => {
+    const downloadImage = document.createElement("a");
+    document.body.appendChild(downloadImage);
+    downloadImage.setAttribute("download", "image");
+    downloadImage.href = img_el.src;
+    downloadImage.click();
+    downloadImage.remove();
+}
+
+copy_btn.onclick = async () => {
+    const res = await fetch(img_el.src);
+    const blob = await res.blob();
+    await navigator.clipboard.write([
+        new ClipboardItem({
+            [blob.type]: blob
+        })
+    ]);
+}
+
+share_btn.onclick = () => {
+    window.open("https://x.com/");
+}
+
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 "use strict";
 import Konva from "konva";
-import type {ITextUnit} from "textalive-app-api";
+import type {ITextUnit, PlayerVideoOptions} from "textalive-app-api";
 
 export function min(a: number, b: number) {
     return a < b ? a : b;
@@ -18,29 +18,29 @@ export const mouse = {
 export const new_up_text = (t: string): Konva.Text => {
     const text = new Konva.Text({
         text: t,
-        scale: {x:.5,y:.5},
+        scale: {x: .5, y: .5},
         fontSize: 120,
-        fontFamily: 'Calibri',
-        fill: 'green',
+        fontFamily: fonts[0],
+        fill: "#000000",
         draggable: true,
         closed: true
         //rotation: 45
     });
     text.offsetX(text.width() / 2);
-    text.offsetY(text.height() / 2 );
+    text.offsetY(text.height() / 2);
     return text;
 }
 
 export class Word {
     text: Konva.Text;
     word: ITextUnit;
-    index:number;
+    index: number;
 
     get time() {
         return this.word.startTime;
     }
 
-    constructor(word: ITextUnit,index:number) {
+    constructor(word: ITextUnit, index: number) {
         this.word = word;
         this.text = new_up_text(word.text);
         this.index = index;
@@ -57,7 +57,7 @@ export class Words {
     times: number[] = [];
     positions: number[] = [];
 
-    get count(){
+    get count() {
         return this.words.length;
     }
 
@@ -112,4 +112,111 @@ export const colors = [
     "#ffee77",
     "#6655cc",
     "#cb213c",
-]
+];
+export const fonts = [
+    "M PLUS 1p",
+    "Sawarabi Mincho",
+    "Dela Gothic One",
+    "Zen Kurenaido"
+];
+
+class Song {
+    title: string
+    url: string;
+    options: PlayerVideoOptions;
+
+    constructor(title: string, url: string, options: PlayerVideoOptions) {
+        this.title = title;
+        this.url = url;
+        this.options = options;
+    }
+}
+
+export const songs = [
+    new Song(
+        "SUPERHERO / めろくる",
+        "https://piapro.jp/t/hZ35/20240130103028",
+        {
+            video: {
+                // 音楽地図訂正履歴
+                beatId: 4592293,
+                chordId: 2727635,
+                repetitiveSegmentId: 2824326,
+                // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FhZ35%2F20240130103028
+                lyricId: 59415,
+                lyricDiffId: 13962
+            },
+        }),
+    new Song(
+        "いつか君と話したミライは / タケノコ少年",
+        "https://piapro.jp/t/--OD/20240202150903",
+        {
+            video: {
+                // 音楽地図訂正履歴
+                beatId: 4592296,
+                chordId: 2727636,
+                repetitiveSegmentId: 2824327,
+                // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2F--OD%2F20240202150903
+                lyricId: 59416,
+                lyricDiffId: 13963
+            },
+        }),
+    new Song(
+        "フューチャーノーツ / shikisai",
+        "https://piapro.jp/t/XiaI/20240201203346",
+        {
+            video: {
+                // 音楽地図訂正履歴
+                beatId: 4592297,
+                chordId: 2727637,
+                repetitiveSegmentId: 2824328,
+                // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FXiaI%2F20240201203346
+                lyricId: 59417,
+                lyricDiffId: 13964
+            },
+        }),
+    new Song(
+        "未来交響曲 / ヤマギシコージ",
+        "https://piapro.jp/t/Rejk/20240202164429",
+        {
+            video: {
+                // 音楽地図訂正履歴
+                beatId: 4592298,
+                chordId: 2727638,
+                repetitiveSegmentId: 2824329,
+                // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FRejk%2F20240202164429
+                lyricId: 59418,
+                lyricDiffId: 13965
+            }
+        }),
+    new Song(
+        "リアリティ / 歩く人",
+        "https://piapro.jp/t/ELIC/20240130010349",
+        {
+            video: {
+                // 音楽地図訂正履歴
+                beatId: 4592299,
+                chordId: 2727639,
+                repetitiveSegmentId: 2824330,
+                // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FELIC%2F20240130010349
+                lyricId: 59419,
+                lyricDiffId: 13966
+            },
+        }),
+    new Song(
+        "The Marks / 2ouDNS",
+        "https://piapro.jp/t/xEA7/20240202002556",
+        {
+            video: {
+                // 音楽地図訂正履歴
+                beatId: 4592300,
+                chordId: 2727640,
+                repetitiveSegmentId: 2824331,
+                // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FxEA7%2F20240202002556
+                lyricId: 59420,
+                lyricDiffId: 13967
+            },
+        }
+    )
+
+];
